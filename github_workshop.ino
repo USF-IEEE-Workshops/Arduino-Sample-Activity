0c16731d93609df3306ee3cdddb370fa013cebf8
USF-IEEE-Workshops <89890889+USF-IEEE-Workshops@users.noreply.github.com>
2021-08-31 23:57:13 -0400

    Blinking LED
    This is a simple Arduino code which we can modify.

---
+++ github_workshop.ino
@@ -0,0 +1,22 @@
+#define LED1 2
+#define LED2 3
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED1,OUTPUT);//set pin 2 as an output
+  digitalWrite(LED1,LOW);//set pin 2 to the "low" or "off" state
+  pinMode(LED2,OUTPUT);//set pin 3 as an output
+  digitalWrite(LED2,LOW);//set pin 2 to the "low" or "off" state
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:  
+  delay(100);//delay measured in miliseconds
+  digitalWrite(LED1,HIGH);//set pin 2 to the "high" or "on" state
+  delay(1000);//delay measured in miliseconds
+  digitalWrite(LED1,LOW);//set pin 2 to the "low" or "off" state
+  delay(100);//delay measured in miliseconds
+  digitalWrite(LED2,HIGH);//set pin 3 to the "high" or "on" state
+  delay(2000);//delay measured in miliseconds
+  digitalWrite(LED2,LOW);//set pin 3 to the "low" or "off" state
+}
